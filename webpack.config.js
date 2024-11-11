6c24f1aac7f8f4704494a238faf1008956947478
alexanderjuxoncobb <77504953+alexanderjuxoncobb@users.noreply.github.com>
2024-11-11 11:01:56 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devtool: "source-map",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
